681f2104e903467e83ead5f729295ac140358665
dariodlearnco <dario.dorella@gmail.com>
2018-01-07 22:44:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,6 @@ function destructivelyRemoveLastKitten() {
     kittens.pop()
 }
 
-function destructivelyRemoveFirstKitten() {}
+function destructivelyRemoveFirstKitten() {
+    kittens.shift()
+}
